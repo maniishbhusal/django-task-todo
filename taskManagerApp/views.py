8b867a9376d20187ce9a3f421128b1723d1357c2
Manish Bhusal <manishbhusal6@gmail.com>
2023-12-18 16:56:31 +0545

    feat: Implement authentication check for home page access

--- taskManagerApp/views.py
+++ taskManagerApp/views.py
@@ -1,8 +1,16 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+@login_required
 def index(request):
-    # return HttpResponse("placeholder to later display a list of all blogs")
-    # return redirect('user_login')
-    return render(request, 'taskManagerApp/index.html')
-
+    try:
+        User = get_user_model()
+        user = User.objects.get(pk=request.user.pk)
+    except Exception as e:
+        # Handle the exception here
+        user = None
+    if user.is_authenticated:
+        return render(request, 'taskManagerApp/index.html')
+    return redirect('user_login')
