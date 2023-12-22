c6e5baf86fe1869bb4544e79d2e150e83db02635
Manish Bhusal <manishbhusal6@gmail.com>
2023-12-22 10:12:40 +0545

    Add render domain to allowed_hosts

--- task_todo/settings.py
+++ task_todo/settings.py
@@ -16,7 +16,8 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['task-todo-app.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
